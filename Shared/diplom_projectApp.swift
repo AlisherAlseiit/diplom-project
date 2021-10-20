cad7b094454e2e7e9b5d01ca68c9a0b7ae9b584e
Alisher <Alisher>
2021-10-20 18:15:10 +0600

    Initial Commit

---
+++ Shared/diplom_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  diplom_projectApp.swift
+//  Shared
+//
+//  Created by Алишер Алсейт on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct diplom_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
